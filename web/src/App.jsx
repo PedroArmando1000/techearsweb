ad3b91cd7855038716af70ddbed4771787ef571e
LacavaSP <deitylink38@gmail.com>
2024-11-21 23:00:51 -0300

    meu comentario

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -11,7 +11,7 @@ import "primereact/resources/themes/lara-light-cyan/theme.css";
 
 import 'primeicons/primeicons.css';
 
-
+// COMENTARIO
 const App = ({ children }) => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
